9441575845b971ba6d93b24782f5b7b470df5371
Mohana Rao Sanapala <65847725+SMohanaRao@users.noreply.github.com>
2025-05-12 23:12:27 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,26 +6,18 @@ bench init --skip-redis-config-generation frappe-bench
 
 cd frappe-bench
 
-# Use containers instead of localhost
 bench set-mariadb-host mariadb
 bench set-redis-cache-host redis://redis:6379
 bench set-redis-queue-host redis://redis:6379
 bench set-redis-socketio-host redis://redis:6379
 
-# Remove redis, watch from Procfile
 sed -i '/redis/d' ./Procfile
 sed -i '/watch/d' ./Procfile
 
 bench get-app erpnext
 bench get-app hrms
 
-bench new-site hrms.localhost \
---force \
---mariadb-root-password root \
---admin-password admin \
---db-host mariadb \
---db-port 3306 \
---verbose
+bench new-site hrms.localhost --force --mariadb-root-password root --admin-password admin --db-host mariadb --db-port 3306 --verbose
 
 bench --site hrms.localhost install-app hrms
 bench --site hrms.localhost set-config developer_mode 1
